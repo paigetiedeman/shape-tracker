0fc6bc0e67d7f0cc5a7694617031b3dac59142ec
Paige Tiedeman <paigetiedeman@gmail.com>
2021-08-31 21:10:45 -0700

    shape tracker with rectangle function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Triangle from './triangle.js';
+import Rectangle from './js/rectangle.js';
+
 
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
@@ -13,5 +15,14 @@ $(document).ready(function() {
     const triangle = new Triangle(length1, length2, length3)
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
+  });
+  
+  $('#rectangle-area-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = parseInt($('#rect-length1').val());
+    const length2 = parseInt($('#rect-length2').val());
+    const rectangle = new Rectangle(length1, length2);
+    const response = rectangle.getArea();
+    $('#response2').append(`<p> The area of the rectangle is ${response}.</p>`);
   });
 });
